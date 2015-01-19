45e702046485c4876f33899ffed757bfffdde732
Md. Iftekhar Tanveer <go2chayan@gmail.com>
2015-01-18 23:36:25 -0500

    First commit. Log likelihood is decreasing but not giving correct result.

---
+++ analyze.py
@@ -0,0 +1,316 @@
+import numpy as np
+import scipy.signal as sg
+import scipy.io as sio
+import fileio as fio
+import matplotlib.pyplot as pp
+
+# This function performs convolution (*) of alpha and psi
+# alpha is N x D (axis-0:time/frame, axis-1: various AEB)
+# alpha represents sparse coefficient for various AEB's
+# psi is M x K x D (axis-1: x, y and z components of AEB)
+# psi represents all the AEBs (D number of them)
+# OUTPUT: returns an N x k x d tensor which is alpha*psi over axis-0
+def convAlphaPsi(alpha,psi):
+    szAlpha = np.shape(alpha)
+    szPsi = np.shape(psi)
+    assert len(szAlpha) == 2 and len(szPsi) == 3 and szAlpha[1] == szPsi[2]
+    convRes = np.zeros((szAlpha[0],szPsi[1],szAlpha[1]))
+    for j in xrange(szAlpha[1]):
+        for i in xrange(szPsi[1]):
+            convRes[:,i,j] = np.convolve(alpha[:,j],psi[:,i,j],'same')
+    return convRes
+
+# Reconstruct the data from components
+# s is d x 1 tensor containing scalar loading for each alpha*psi
+# alpha is N x D, psi is M x K X D
+# OUTPUT: inner product of alpha*psi and s
+def recon(alpha,psi):
+    szAlpha = np.shape(alpha)
+    szPsi = np.shape(psi)
+    assert len(szAlpha) == 2 and len(szPsi) == 3 and szAlpha[1] == szPsi[2]
+    convRes = convAlphaPsi(alpha,psi)
+    return np.sum(convRes,axis=2)
+
+def calcP(X,alpha,psi):
+    L = recon(alpha,psi)
+    return (0.5*np.sum((X-L)**2.))
+
+# TODO: Check it carefully    
+def calcObjf(X,alpha,psi,beta):
+    L = recon(alpha,psi)
+    return (0.5*np.sum((X-L)**2.) + beta*np.sum(np.abs(alpha)))
+
+# X_,L_,alpha_ are of X(:,:,i), L(:,:,i) and alpha(:,i)
+def loglike(X,alpha,psi,beta):
+    return np.log(calcObjf(X,alpha,psi,beta))
+    #return (0.5*np.sum((X-L)**2.))
+
+## Deprecated: Requires too much memory
+## Builds the Toeplitz tensor from alpha for convolution
+## Returns a NxMxD tensor which represents gradient of L (f according to report)
+## with respect to psi. grad_{psi_t'}(f(t)) = alpha(t-t')
+## So, this is just the value of alpha with proper shifts
+#def buildToeplitz_psi(alpha, M):
+#    N,D = np.shape(alpha)
+#    gradVal = np.zeros((N,M,D))
+#    for d in xrange(D):
+#        gradVal[:,0,d] = alpha[:,d].copy()
+#        for m in xrange(M-1):
+#            gradVal[1:,m+1,d] = gradVal[:-1,m,d].copy()
+#    return gradVal
+
+## Deprecated: Requires too much Memory
+## Builds the Toeplitz tensor from psi for convolution
+## Returns a NxNxDxK tensor which represents gradient of L (f according to report)
+## with respect to psi. grad_{alpha_t'}(f(t)) = psi(t-t')
+## So, this is just the value of alpha with proper shifts
+#def buildToeplitz_alpha(psi, N):
+#    M,K,D = np.shape(psi)
+#    gradVal = np.zeros((N,N,D,K))
+#    for d in xrange(D):
+#        gradVal[:M,0,d,:] = psi[:,:,d]
+#        for n in xrange(N-1):
+#            gradVal[1:,n+1,d,:] = gradVal[:-1,n,d,:].copy()
+#    return gradVal
+#
+## Deprecated: dependent on deprecated Toeplitz    
+## Grad of P wrt alpha is sum((X(t)-L(t))psi(t-t'))
+## Returns an NxD tensor representing gradient of P with respect to psi
+#def calcGrad_alpha(alpha,psi,lxDiff):
+#    N,D = np.shape(alpha)
+#    M,K,_ = np.shape(psi)
+#    gradL_alpha = buildToeplitz_alpha(psi,N) # NxNxDxK tensor
+#    gradP_alpha = np.zeros((N,D,K))
+#    for k in xrange(K):    
+#        gradP_alpha[:,:,k] = np.tensordot(gradL_alpha[:,:,:,k],lxDiff[:,k],[0,0])
+#    gradP_alpha = np.sum(gradP_alpha,axis=2)
+#    return gradP_alpha
+
+# Manually Checked with sample data -- Working as indended
+# Grad of P wrt alpha is sum((X(t)-L(t))psi(t-t'))
+# Returns an NxD tensor representing gradient of P with respect to psi
+def calcGrad_alpha(alpha,psi,lxDiff):
+    N,D = np.shape(alpha)
+    M,K,_ = np.shape(psi)
+    #gradL_alpha = buildToeplitz_alpha(psi,N) # NxNxDxK tensor
+    gradP_alpha = np.zeros((N,D,K))
+    for d in xrange(D):
+        for k in xrange(K):
+            # this is actually a correlation operation. Notice that 
+            # psi is actually flipped
+            gradP_alpha[:,d,k] = \
+            sg.fftconvolve(lxDiff[:,k],psi[::-1,k,d],'full')[(M-1):]
+    gradP_alpha = np.sum(gradP_alpha,axis=2)/K  # Should we really avg?
+    return gradP_alpha
+
+# Manually Checked with sample data -- Working as indended
+# Grad of P wrt psi is sum((X(t)-L(t))alpha(t-t'))
+# Returns an MxKxD tensor representing gradient of P with respect to psi
+def calcGrad_psi(alpha,psi,lxDiff):
+    N,D = np.shape(alpha)
+    M,K,_ = np.shape(psi)
+    gradP_psi = np.zeros((M,K,D))
+    for d in xrange(D):    
+        for k in xrange(K):
+            # this is actually a correlation operation. Notice that 
+            # alpha is actually flipped
+            gradP_psi[:,k,d] = \
+            sg.fftconvolve(lxDiff[:,k],alpha[::-1,d],'full')[(N-1):(N+M-1)] 
+    return gradP_psi
+    
+# Randomly initialize alpha and psi
+# alpha is N x D (axis-0:time/frame, axis-1: various AEB)
+# alpha represents sparse coefficient for various AEB's
+# psi is M x K x D (axis-1: x, y and z components of AEB)
+# psi represents all the AEBs (D number of them)
+# M is a scalar integer representing time/frame length for AEB 
+def csc_init(M,N,K,D):
+    psi = normalizePsi(np.random.randn(M,K,D))
+    #alpha = np.zeros((N,D))
+    alpha = np.random.randn(N,D)
+    return psi,alpha
+
+#Normalize psi    
+def normalizePsi(psi):
+    M,K,D = np.shape(psi)
+    for d in xrange(D):
+        for k in xrange(K):
+            psiNorm = np.linalg.norm(psi[:,k,d])
+            if psiNorm !=0:
+                psi[:,k,d] = psi[:,k,d]/psiNorm
+    return psi
+
+# Apply Proximal/Shrinkage operation
+def shrink(operateOn, threshold):
+        idx_z = np.where(np.abs(operateOn)<=threshold)
+        idx_nz = np.where(np.abs(operateOn)>threshold)
+        operateOn[idx_z] = 0.
+        operateOn[idx_nz] -= threshold*np.sign(operateOn[idx_nz])
+        return operateOn
+
+# Plots alpha, psi, original and reconstructed data
+def dispPlots(alpha,psi,X,figureName):
+    _,D = np.shape(alpha)
+    pp.figure(figureName)
+    pp.clf()
+
+    pp.subplot(411)
+    pp.plot(X)
+    pp.title('Original Data')
+    
+    pp.subplot(412)
+    L = recon(alpha,psi)
+    pp.plot(L)
+    pp.title('Reconstructed Data')
+
+    pp.subplot(413)    
+    for d in xrange(D):
+        pp.plot(psi[:,:,d])
+    pp.title('psi')
+    
+    pp.subplot(414)        
+    for d in xrange(D):
+        pp.stem(alpha[:,d])
+    pp.title('alpha')
+
+# Applies Convolutional Sparse Coding with Proximal Gradient Descent Algorithm    
+# X is N x K data tensor for only one joint 
+# M is a scalar integer representing time/frame length for AEB     
+# D represents how many AEB we want to capture
+# beta is the weight for sparcity constraint
+def csc_pgd(X,M,D,beta,iter_thresh=250,thresh = 1e-4):
+    iter = 0
+    N,K = np.shape(X)
+    psi,alpha = csc_init(M,N,K,D)    # Random initialization
+        
+    factor = 0.75
+    prevlikeli = loglike(X,alpha,psi,beta)
+    firstIter = True
+    firstDeltaLikeli = 0
+    countZero = 0
+    while iter < iter_thresh:
+        L = recon(alpha,psi)
+        
+        # calculate the error
+        lxDiff = L - X
+        
+        # Calculate the gradients
+        grpsi = calcGrad_psi(alpha,psi,lxDiff)
+        gralpha = calcGrad_alpha(alpha,psi,lxDiff)        
+
+        # 2D Line Search: Update psi and alpha        
+#        gammaAlpha = 1.0
+#        gammaPsi = 1.0
+#        oldP = calcObjf(X,alpha,psi,beta)
+#        foundVal = False
+#        while gammaPsi > 5e-10:
+#            while gammaAlpha > 5e-10:
+#                newPsi = normalizePsi(psi - gammaPsi*grpsi)
+#                newAlpha = alpha - gammaAlpha*gralpha
+#    #            print 'Trying Gamma = ', gammaK
+#                if calcObjf(X,newAlpha,newPsi,beta)>=oldP:
+#                    gammaAlpha *= factor
+#                else:
+#    #                print 'Gamma = ', gammaK
+#                    foundVal = True
+#                    break
+#            if foundVal:
+#                break
+#            gammaPsi*=factor
+        
+        # 1 D Line Search: Update psi and alpha
+#        gamma = 1.0
+#        oldP = calcObjf(X,alpha,psi,beta)
+#        while gamma > 5e-10:
+#            newPsi = normalizePsi(psi - gamma*grpsi)
+#            newAlpha = alpha - gamma*gralpha
+#            if calcObjf(X,newAlpha,newPsi,beta)>=oldP:
+#                 gamma *= factor
+#            else:
+#                 break
+             
+        # No Line search: Linear decrease of learning rate
+        gamma = 0.001/(iter+1)
+        newPsi = normalizePsi(psi - gamma*grpsi)
+        newAlpha = alpha - gamma*gralpha
+        
+
+        # Shrinkage operation on alpha
+        newAlpha = shrink(newAlpha,beta*gamma)
+
+        # Make the update permanent
+        psi = newPsi
+        alpha = newAlpha
+        iter += 1
+        
+        # Display        
+        likeli = loglike(X,alpha,psi,beta)
+        #dispPlots(alpha,psi,X,'Iteration Data')
+        pp.figure('Log likelihood plot')
+        pp.scatter(iter,likeli,c = 'b')
+        pp.draw()
+        pp.pause(0.05)
+        
+        # terminate loop
+        allowZero_number = 15
+        if firstIter:
+           firstIter = False
+           firstDeltaLikeli = prevlikeli - likeli
+        print 'Gamma_ = ', gamma,\
+        ' likeli = ', likeli, ' delta = ', prevlikeli - likeli, \
+        '({:.2f}%)'.format((prevlikeli - likeli)/firstDeltaLikeli*100)
+        if abs(prevlikeli - likeli)<0.00001:#*firstDeltaLikeli:
+            if abs(prevlikeli - likeli) <0.00001 and countZero < allowZero_number:
+                countZero += 1
+            elif abs(prevlikeli - likeli) <0.00001 and countZero >= allowZero_number:
+                break
+            print countZero
+        else:
+            prevlikeli = likeli
+    return alpha,psi
+        
+def main():
+#    # read joint names and data file
+#    jointID = fio.readPointDic('Data/pointDef.dic')    
+#    #dat,timeIndx,_ = readDataFile('Data/','.csv',joints=(\
+#    # jointID['ELBOW_RIGHT'],jointID['WRIST_RIGHT']),preprocess=True)
+#    #dat,boundDic = fio.readAllFiles('Data/','.csv',preprocess=True)
+#    #sio.savemat('Data/jointInfo.mat',{'dat':dat})
+#    dat = sio.loadmat('Data/jointInfo.mat')['dat']
+#    
+#    #TODO: Delete this line. For debug only
+#    dat = dat[:500,:,:]
+#    
+#    # apply Shift Invariant Sparse Coding. 
+#    # Length of AEB is set to 2 seconds (60 frames)
+#     # attempt to model only one joint for now    
+#    X = dat[:,:,jointID['WRIST_RIGHT']]
+    alpha,psi = fio.toyExample_small()
+    X = recon(alpha,psi)
+    # Display original data
+    pp.figure('Original alpha, psi and Data')
+    pp.clf()
+    pp.subplot(311)
+    for i in xrange(3):
+        pp.plot(psi[:,i])
+    pp.title('psi')
+    pp.subplot(312)
+    pp.stem(alpha)
+    pp.title('alpha')
+    pp.subplot(313)
+    for i in xrange(3):
+        pp.plot(X[:,i])
+    pp.title('X')
+    pp.draw()
+    pp.pause(0.1)
+    # D represents how many AEB we want to capture
+    D = 1
+    (alpha_recon,psi_recon) = csc_pgd(X,M=(len(psi)),D=D,beta=0.01)
+    
+    # Display the reconstructed values
+    dispPlots(alpha_recon,psi_recon,X,'Final Data')
+    pp.show()
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ fileio.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 15 00:49:26 2014
+
+@author: Md.Iftekhar
+"""
+import numpy as np
+import os
+
+#################################### FILE IO ##################################
+# read point definition file
+def readPointDic(dicFilename):
+    with open(dicFilename) as f:
+        pointName = [dat.strip().split('=') for dat in f.readlines()]
+        points = dict((elem[0].strip(),int(elem[1])) for elem in pointName)
+        return points 
+
+# returns a tensor containing the kinect skeleton data and corresponding 
+# timestamp (in Second) for each row. Also the screen coordinates.
+def readDataFile(readDataFile,joints):
+    # read datafile
+    with open(readDataFile) as f:
+        temp = [x.split(',') for x in f.readlines()]
+    # Convert the data into a convenient numpy array
+    dat = np.array([x[0:-1] for x in temp]).astype(np.float)
+    timeStampSec = (dat[:,0] - dat[0,0])/300    # copy the timestamp
+    dat = np.delete(dat,0,axis=1)               # Delete timestamp column
+    # Copy x and y locations of the points
+    screenXY = dat[:,(np.arange(101)%5==3) | (np.arange(101)%5==4)]
+    # reshape the data in a higher dimensional tensor
+    m,n = dat.shape
+    dat.shape = (m,20,5)
+    dat = np.delete(dat,(3,4),axis=2)
+    if not joints:
+        joints = range(20)
+    return dat[:,joints,:],timeStampSec,screenXY
+
+# We delete last nSec * 30 frames because that is usually garbage
+def clean(dat,nSec = 10):
+    return dat[:-(30*nSec),:,:]
+    
+# Normalization of the skeleton data. x,y,z components are individually
+# meansubtracted and divided by
+def normalizeDat(dat):    
+    datMean = np.mean(dat,axis=0)
+    dat = (dat - datMean)
+    datStd = np.max(np.std(dat,axis=0),axis=1)
+    for j in xrange(np.size(dat,axis=1)):
+        dat[:,j,:]/=datStd[j]
+    return dat
+
+# Reads all the skeleton tracking file in a folder and concatenates to 
+# to a single 3 dimensional array. First dimension is for frame number. 2nd and 
+# 3rd dimension is for jointID and (x,y,z) values respectively 
+# returns a tensor containing the data from all files
+# Applies some rudimentary preprocessing if preprocess is turned on
+def readAllFiles(fullFiles,suffix,preprocess,joints=(),nSec = 10):
+    if not joints:
+        m = 20
+    else:
+        m = len(joints)
+    allData = np.zeros((1,m,3))
+    boundDic = {}
+    bound = (-1,-1)
+    for root,folder,files in os.walk(fullFiles):
+        for afile in files:
+            if afile.lower().endswith(suffix.lower()):
+                # create full filename and print
+                fullPath = os.path.join(root,afile)
+                print fullPath
+                # Read file. 
+                dat,_,_ = readDataFile(fullPath,joints)
+                # Preprocess
+                if preprocess:
+                    dat = normalizeDat(clean(dat,nSec))
+                # Keep a record of where each file starts and ends 
+                bound = (bound[1]+1,bound[1]+np.size(dat,axis=0))
+                boundDic[bound] = fullPath
+                # Concatenate all the data
+                allData = np.concatenate((allData,dat),axis=0)
+    allData=np.delete(allData,0,axis=0)
+    allData = np.transpose(allData,axes=[0,2,1])
+    return allData,boundDic
+
+# Given any row index and the doundary dictionary it returns from which
+# file the row is coming. Important to trace back for finding the frame in
+# the video
+def findWhichFile(rowIndx,boundDic):
+    rowNumber = np.where((np.array(boundDic.keys())[:,0]<rowIndx) & \
+    (np.array(boundDic.keys())[:,1]>rowIndx))[0][0]
+    return boundDic.values()[rowNumber]
+    
+# Generate and return a toy data
+def toyExample_large():
+    alpha = np.zeros((5000,1))
+    alpha[500] = 1
+    alpha[2500] = 0.5
+    alpha[3000] = -1
+    xVal = np.arange(-np.pi,np.pi,0.0075)
+    psi = np.zeros((len(xVal),3,1))
+    psi[:,0,0] = np.sin(np.sin(xVal))
+    psi[:,1,0] = np.sin(np.sin(np.pi + xVal))
+    psi[:,2,0] = np.sin(np.sin(np.pi/4 + xVal))
+    return alpha,psi
+
+# Generate and return a toy data
+def toyExample_small():
+    alpha = np.zeros((10,1))
+    alpha[3] = 1
+    alpha[7] = -0.5
+    psi = np.zeros((3,3,1))
+    psi[:,0,0] = [1,2,1]
+    psi[:,1,0] = [-1,-2,-1]
+    psi[:,2,0] = [-1,1,-1]
+    return alpha,psi
+############################## End File IO ####################################
